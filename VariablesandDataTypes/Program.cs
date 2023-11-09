3e20f3d74b29290e7225ce368486050d2f26d831
Maheshvema <babumahesh2088@gmail.com>
2023-11-09 14:41:24 +0530

    modified

---
+++ VariablesandDataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesandDataTypes
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            string Name = "Mahesh";
+            int Age = 23;
+            double Height = 144.56;
+            Console.WriteLine($"Name:{Name}\tAge:{Age}\tHeight\t{Height}\t");
+            Console.ReadKey();
+        }
+    }
+}
